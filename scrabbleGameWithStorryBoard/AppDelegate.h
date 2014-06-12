7fa398ecb58cd0168cf2897dc70043d81dc2d5bc
clevin95 <carter.levin@flatironschool.com>
2014-06-12 14:18:32 -0400

    Initial Commit

---
+++ scrabbleGameWithStorryBoard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  scrabbleGameWithStorryBoard
+//
+//  Created by swift on 6/12/14.
+//  Copyright (c) 2014 swift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ scrabbleGameWithStorryBoard/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  scrabbleGameWithStorryBoard
+//
+//  Created by swift on 6/12/14.
+//  Copyright (c) 2014 swift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
